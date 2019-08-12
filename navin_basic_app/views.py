5b37d8ad440eeb9c3418751a318dd760514b06fd
BharathRao Burannagari <bharathrao.burannagari@coxautoinc.com>
2019-08-12 18:14:14 -0400

    The home_page html is added and the view is redirected in there. Thank you.

--- navin_basic_app/views.py
+++ navin_basic_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# def home_page(request):
+#     return HttpResponse("Hello, All This is the website of Mr. Naveen Kumar Reddy Chappidi. I am working in IT industry from couple of year. I am an highly qualified individual in developent of websites and web projects.")
 def home_page(request):
-    return HttpResponse("Hello, All This is the website of Mr. Naveen Kumar Reddy Chappidi. I am working in IT industry from couple of year. I am an highly qualified individual in developent of websites and web projects.")
+    return render(request, "home_page.html")
